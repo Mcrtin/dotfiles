05a83590c86bfa20e98348a3175ee92d199328f6
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2024-03-08 18:18:03 +0100

    fix permissions for the last time

--- install.sh
+++ install.sh
@@ -23,33 +23,38 @@ sudoFunc () {
 
     # Configure X11
     cp -rT ./etc /etc
-    # Copy dotfiles
-    cp -rT .config $HOMEDIR/.config
+    
 
     # Install packages
     pacman -S --needed man-db tldr git base-devel neovim qutebrowser translate-shell brightnessctl pavucontrol rofi aerc lazygit tmux dunst alacritty ttf-jetbrains-mono-nerd fprintd copyq
 
-    # Install LazyVim
-    git clone https://github.com/LazyVim/starter $HOMEDIR/.config/nvim
-    rm -rf $HOMEDIR/.config/nvim/.git
-
-
-    # Install yay
-    git clone https://aur.archlinux.org/yay-bin.git $SCRIPT_DIR/yay-bin && (cd yay-bin && su nobody -c "makepkg -si")
-
-    yay -S spotify-tui catppuccin-gtk-theme-mocha libinput-gestures --noconfirm
-
-    # Install Catppuccin for Rofi
-    git clone https://github.com/catppuccin/rofi.git $SCRIPT_DIR/rofi
-    bash rofi/basic/install.sh
-
-    # Install Catppuccin for alacritty
-    curl -LOC --output-dir $HOMEDIR/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
-    # Install Catppuccin for qute
-    git clone https://github.com/catppuccin/qutebrowser.git $HOMEDIR/.config/qutebrowser/catppuccin
-    git clone https://github.com/catppuccin/spotify-tui.git && cp spotify-tui/mocha.yml $HOMEDIR/.config/spotify-tui/
+    yay -S spotify-tui catppuccin-gtk-theme-mocha libinput-gestures
 
 }
 
 FUNC=$(declare -f sudoFunc)
 sudo -H bash -c "$FUNC; sudoFunc $*;"
+
+
+# Copy dotfiles
+cp -rT .config $HOMEDIR/.config 
+
+# Install LazyVim
+git clone https://github.com/LazyVim/starter $HOMEDIR/.config/nvim
+rm -rf $HOMEDIR/.config/nvim/.git
+
+
+# Install yay
+git clone https://aur.archlinux.org/yay-bin.git $SCRIPT_DIR/yay-bin && (cd yay-bin && makepkg -si)
+
+# Install Catppuccin for Rofi
+git clone https://github.com/catppuccin/rofi.git $SCRIPT_DIR/rofi
+bash rofi/basic/install.sh
+
+
+# Install Catppuccin for alacritty
+curl -LOC --output-dir $HOMEDIR/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+# Install Catppuccin for qute
+git clone https://github.com/catppuccin/qutebrowser.git $HOMEDIR/.config/qutebrowser/catppuccin
+# Install Catppucccin for spotify-tui
+git clone https://github.com/catppuccin/spotify-tui.git $SCRIPT_DIR/spotify-tui && cp spotify-tui/mocha.yml $HOMEDIR/.config/spotify-tui/
