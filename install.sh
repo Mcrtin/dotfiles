012423b6606efe9efd20456f2ff020214995711d
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2024-03-09 16:11:41 +0100

    only add config options if they aren't already there

--- install.sh
+++ install.sh
@@ -30,15 +30,19 @@ sudoFunc () {
     systemctl enable systemd-resolved.service 
     systemctl enable iwd.service
     
-    # Instaall Catppuccin for grub
-    cp -r grub/src/* /usr/share/grub/themes/
-    echo "GRUB_THEME=\"/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt\"" >> /etc/default/grub
-    grub-mkconfig -o /boot/grub/grub.cfg
+    
 
     # Install packages
     pacman -S --needed man-db tldr git base-devel neovim qutebrowser translate-shell brightnessctl spotifyd pulseaudio pavucontrol rofi aerc lazygit tmux dunst alacritty ttf-jetbrains-mono-nerd fprintd copyq lightdm-webkit2-greeter xorg-xdpyinfo
 
-    echo "GRUB_GFXMODE=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')" >> /etc/default/grub
+    if [[$(tail -1 /etc/default/grub) == "GRUB_GFXMODE"*]];
+    then
+        # Instaall Catppuccin for grub
+        cp -r grub/src/* /usr/share/grub/themes/
+        echo "GRUB_THEME=\"/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt\"" >> /etc/default/grub
+        echo "GRUB_GFXMODE=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')" >> /etc/default/grub
+        grub-mkconfig -o /boot/grub/grub.cfg
+    fi
 }
 
 FUNC=$(declare -f sudoFunc)
