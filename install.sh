3ecc3e4fdfb225dc72f6dc86ffb10515ae473ebe
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2024-03-09 09:09:36 +0100

    only install yay if needed

--- install.sh
+++ install.sh
@@ -35,6 +35,13 @@ sudoFunc () {
 FUNC=$(declare -f sudoFunc)
 sudo -H bash -c "$FUNC; sudoFunc $*;"
 
+if ! command -v yay &> /dev/null
+then
+    # Install yay
+    git clone https://aur.archlinux.org/yay-bin.git && (cd yay-bin && makepkg -si)
+fi
+
+
 yay -S --needed spotify-tui catppuccin-gtk-theme-mocha libinput-gestures --noconfirm
 
 
@@ -46,16 +53,13 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 
-# Install yay
-git clone https://aur.archlinux.org/yay-bin.git && (cd yay-bin && makepkg -si)
-
 # Install Catppuccin for Rofi
 git clone https://github.com/catppuccin/rofi.git
 bash rofi/basic/install.sh
 
 
 # Install Catppuccin for alacritty
-curl -LOC --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
 # Install Catppuccin for qute
 git clone https://github.com/catppuccin/qutebrowser.git ~/.config/qutebrowser/catppuccin
 # Install Catppucccin for spotify-tui
